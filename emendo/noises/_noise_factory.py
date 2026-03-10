c77fe312d7ddb42165751e4b7f1be505d2aeaf70
Nicholas Kania <nicholas.nick.kania.development@gmail.com>
2026-03-11 00:46:44 +0100

    Testing
    > Added `ValueError` check in `NoiseFactory` if configuration is None
    > Added `KeyError` check in `NoiseFactory` if selected noise does not exist

--- emendo/noises/_noise_factory.py
+++ emendo/noises/_noise_factory.py
@@ -13,5 +13,8 @@ class NoiseFactory:
 
     @staticmethod
     def create(cfg: NoiseConfig) -> Noise:
+        if cfg is None:
+            raise ValueError("Configuration can't be `None`")
+        
         cls = NoiseFactory.__map[cfg.name]
         return cls(cfg)
--- tests/noise/test_noise_factory.py
+++ tests/noise/test_noise_factory.py
@@ -4,4 +4,72 @@ import pytest
 
 # Testing
 class TestNoiseFactory:
-    pass
+    def test_white_noise_creation(self):
+        cfg = E.noises.NoiseConfig(
+            name    = "white",
+            freq    = 100.0,
+            scale   = 1.0,
+            seed    = 42
+        )
+        noise = E.noises.NoiseFactory.create(cfg)
+        
+        assert isinstance(noise, E.noises.WhiteNoise)
+    
+    def test_brown_noise_creation(self):
+        cfg = E.noises.NoiseConfig(
+            name    = "brown",
+            freq    = 100.0,
+            scale   = 1.0,
+            seed    = 42
+        )
+        noise = E.noises.NoiseFactory.create(cfg)
+        
+        assert isinstance(noise, E.noises.BrownNoise)
+    
+    def test_pink_noise_creation(self):
+        cfg = E.noises.NoiseConfig(
+            name    = "pink",
+            freq    = 100.0,
+            scale   = 1.0,
+            seed    = 42
+        )
+        noise = E.noises.NoiseFactory.create(cfg)
+        
+        assert isinstance(noise, E.noises.PinkNoise)
+    
+    def test_blue_noise_creation(self):
+        cfg = E.noises.NoiseConfig(
+            name    = "blue",
+            freq    = 100.0,
+            scale   = 1.0,
+            seed    = 42
+        )
+        noise = E.noises.NoiseFactory.create(cfg)
+        
+        assert isinstance(noise, E.noises.BlueNoise)
+    
+    def test_violet_noise_creation(self):
+        cfg = E.noises.NoiseConfig(
+            name    = "violet",
+            freq    = 100.0,
+            scale   = 1.0,
+            seed    = 42
+        )
+        noise = E.noises.NoiseFactory.create(cfg)
+        
+        assert isinstance(noise, E.noises.VioletNoise)
+        
+    def test_wrong_noise_name(self):
+        cfg = E.noises.NoiseConfig(
+            name    = "green",
+            freq    = 100.0,
+            scale   = 1.0,
+            seed    = 42
+        )
+        
+        with pytest.raises(KeyError):
+            _ = E.noises.NoiseFactory.create(cfg)
+            
+    def test_none_config(self):
+        with pytest.raises(ValueError):
+            _ = E.noises.NoiseFactory.create(None)
